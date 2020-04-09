deecf65d204e46ec58288833f63da85050999e9c
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 01:37:31 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,18 +1,17 @@
-class Appointment 
-  attr_accessor :date
-  attr_reader :patient, :doctor
-  
+class Appointment
   @@all = []
+  attr_reader :patient, :doctor 
+  attr_accessor :date 
   
   def initialize(date, patient, doctor)
     @date = date 
     @patient = patient
     @doctor = doctor
-    @@all << self 
-  end 
+    @@all << self
+  end
   
   def self.all 
-    @@all
-  end 
+    @@all 
+  end
   
-end 
+end
